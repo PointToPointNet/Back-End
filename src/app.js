da071bb96892705a82000cc924df6651ac10a68d
BaeJuh <ghkslqlsl04@gmail.com>
2025-02-17 18:44:06 +0900

    develop setting

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const { port } = require("./config/daemon.mjs");
+const connectionInfo = require("./database/info.mjs");
+const sqlQuery = require("./database/queries.mjs");
+const express = require("express");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection(connectionInfo);
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.get("/database", (req, res) => {
+    connection.query(sqlQuery.select, (err, data) => {
+        if (err) throw err;
+        res.json(data);
+    });
+});
+
+app.listen(port, () => {
+    console.log("docker express start");
+});
