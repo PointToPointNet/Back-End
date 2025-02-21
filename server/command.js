3d23cc90c3f0bef0b8ff1790d28326f8637b2973
BaeJuh <ghkslqlsl04@gmail.com>
2025-02-21 19:48:08 +0900

    exec code

--- server/command.js
+++ server/command.js
@@ -5,39 +5,52 @@ class Commands {
     }
     init() {
         const { exec } = require('child_process');
-        this.exec = exec.bind(require('child_process')); // `exec`을 바인딩
+        this.exec = exec; // `exec`을 바인딩
     }
     // ### ifconfig
-    ifconfig() {
-        this.exec("ifconfig", (err, stdout, stderr) => {
-            if (err || stderr) throw err || stderr;
-            console.log(stdout);
+    // ifconfig() {
+    //     this.exec("ifconfig", (err, stdout, stderr) => {
+    //         if (err || stderr) throw err || stderr;
+    //         console.log(stdout);
+    //         // return stdout.trim();
+    //     });
+    //     return (`
+    //         enp0s25: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+    //         inet 110.5.238.107  netmask 255.255.255.128  broadcast 110.5.238.127
+    //         inet6 fe80::f900:ed1d:473c:e9d0  prefixlen 64  scopeid 0x20<link>
+    //         ether f0:de:f1:45:d1:33  txqueuelen 1000  (Ethernet)
+    //         RX packets 31499952  bytes 14872699632 (14.8 GB)
+    //         RX errors 0  dropped 173  overruns 0  frame 0
+    //         TX packets 44796728  bytes 42813807568 (42.8 GB)
+    //         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+    //         device interrupt 20  memory 0xf2600000-f2620000  
+
+    //         lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
+    //         inet 127.0.0.1  netmask 255.0.0.0
+    //         inet6 ::1  prefixlen 128  scopeid 0x10<host>
+    //         loop  txqueuelen 1000  (Local Loopback)
+    //         RX packets 39109789  bytes 29185449490 (29.1 GB)
+    //         RX errors 0  dropped 0  overruns 0  frame 0
+    //         TX packets 39109789  bytes 29185449490 (29.1 GB)
+    //         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+    //     `).trim();
+    // }
+    async ifconfig() {
+        return new Promise((resolve, reject) => {
+            this.exec("ifconfig", (err, stdout, stderr) => {
+                if (err || stderr) {
+                    reject(err || stderr);
+                    return;
+                }
+                resolve(stdout.trim());
+            });
         });
-        return (`
-            enp0s25: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
-            inet 110.5.238.107  netmask 255.255.255.128  broadcast 110.5.238.127
-            inet6 fe80::f900:ed1d:473c:e9d0  prefixlen 64  scopeid 0x20<link>
-            ether f0:de:f1:45:d1:33  txqueuelen 1000  (Ethernet)
-            RX packets 31499952  bytes 14872699632 (14.8 GB)
-            RX errors 0  dropped 173  overruns 0  frame 0
-            TX packets 44796728  bytes 42813807568 (42.8 GB)
-            TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
-            device interrupt 20  memory 0xf2600000-f2620000  
-    
-            lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
-            inet 127.0.0.1  netmask 255.0.0.0
-            inet6 ::1  prefixlen 128  scopeid 0x10<host>
-            loop  txqueuelen 1000  (Local Loopback)
-            RX packets 39109789  bytes 29185449490 (29.1 GB)
-            RX errors 0  dropped 0  overruns 0  frame 0
-            TX packets 39109789  bytes 29185449490 (29.1 GB)
-            TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
-        `).trim();
     }
 
-    ifconfigToJSON() {
+    async ifconfigToJSON() {
         const resultObj = {};
-        const slicedNetwork = this.ifconfig().trim().split("\n\n").map((network) => network.split("\n").map((line) => line.trim()));
+        const ifconfig = await this.ifconfig();
+        const slicedNetwork = ifconfig.trim().split("\n\n").map((network) => network.split("\n").map((line) => line.trim()));
 
         slicedNetwork.forEach((oneNetwork) => {
             const networkName = oneNetwork[0].split(":")[0].trim();
@@ -53,7 +66,7 @@ class Commands {
                 }
             });
         });
-        // console.log(resultObj);
+        console.log(resultObj);
         return resultObj;
     }
 
