73bc5e6676815a6745a2a26ddce87c5d68898eeb
BaeJuh <ghkslqlsl04@gmail.com>
2025-02-26 13:04:29 +0900

    fix gpt

--- src/route/chat_bot.js
+++ src/route/chat_bot.js
@@ -18,8 +18,12 @@ class ChatBot {
     }
 
     async callChatBot(req, res, question = "") {
+        require("dotenv").config();
         const OpenAI = require("openai");
-        const openai = new OpenAI();
+        // console.log(first)
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY
+        });
         try {
             const completion = await openai.chat.completions.create({
                 model: "gpt-4o-mini",
